8cb0a0f3cb3d8d809fb7d388afbe11eeb5627a1f
Anubhav Joshi <48016317+anubhav-qt@users.noreply.github.com>
2024-08-05 22:32:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ def createFolder(path,fileExtension):
     os.makedirs(folderPath,exist_ok=True) # Create a folder for the file type.
 
 def moveFile(filePath, destinationFolderPath):
-    shutil.move(filePath, destinationFolderPath) # Move the file to the folder.
+    try:
+        shutil.move(filePath, destinationFolderPath)  # Move the file to the folder.
+    except Exception as e:
+        print(f"Error moving file {filePath}: {e}")
 
 def sortFiles(folderPath):
     if not os.path.isdir(folderPath): # Check whether the path exists or not.
-        return "The folder does not exist."
+        print("The folder does not exist.")
+        return
     
     for item in os.listdir(folderPath): # A loop to check every file in the folder.
         itemPath = os.path.join(folderPath,item)
@@ -19,8 +23,11 @@ def sortFiles(folderPath):
             fileExtension = item.split(".")[-1].lower() # Get the file extension and make it all lower case for uniformity purpose.
             newFolderPath = os.path.join(folderPath,fileExtension) # Get the new folder path.
             if not os.path.exists(newFolderPath): # Check whether the folder for the file type exists or not.
-                createFolder(newFolderPath,fileExtension)
+                createFolder(folderPath,fileExtension)
             moveFile(itemPath,newFolderPath) # If folder exists, move the file into the folder.
+    
+    print("All files are sorted!")
+    return
 
 if __name__ == "__main__":
     folderPath = input("Enter the folder path:")
